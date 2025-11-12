10327f952b0e84a750af300d606bc6ddc061956f
gwangho-yi <dlrhkdgh21@gmail.com>
2025-11-12 22:04:11 +0900

    제네릭 - 제네릭 타입

---
+++ src/main/java/practice/generic/Box.java
@@ -0,0 +1,15 @@
+package practice.generic;
+
+public class Box<T> {
+
+    private T object;
+
+    public void set(T object) {
+        this.object = object;
+    }
+
+    public T get() {
+        return object;
+    }
+
+}
--- src/main/java/practice/generic/Main.java
+++ src/main/java/practice/generic/Main.java
@@ -1,13 +1,9 @@
 package practice.generic;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
-        list.add("hello");
-        String s = (String) list.get(0);
-        Integer a = (Integer) list.get(0); // 컴파일 시 오류 발생
+        Box<Integer> box = new Box<Integer>();
+        System.out.println(box.get());
     }
 }
