1e35e40954603918fbb84b0684ed9a67c413468d
gwangho-yi <dlrhkdgh21@gmail.com>
2025-11-22 19:38:57 +0900

    제네릭 - 다중 제한

---
+++ src/main/java/practice/generic/boundedtypeparameters/Main.java
@@ -0,0 +1,26 @@
+package practice.generic.boundedtypeparameters;
+
+import practice.generic.Box;
+
+/**
+ * <h2>제한된 타입 매개변수 (Bounded Type Parameters)</h2>
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        NaturalNumber<Number> naturalNumber = new NaturalNumber<>(10.1);
+        boolean isEven = naturalNumber.isEven();
+        System.out.println("isEven = " + isEven);
+    }
+
+    class A{}
+    interface B{}
+    interface C{}
+
+    class D <T extends A & B & C>{
+
+    }
+
+}
